1dec0c524b90f06cba1489c59befc59025a0cece
julio cesar <jcmcf2012@gmail.com>
2024-01-05 12:57:57 -0300

    feat: add vercel config

---
+++ src/config/connect.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// Database connection URL
+const dbUrl = process.env.DB_KEY;
+
+// Connect to the database
+const connectToMongo = () => {
+  mongoose.connect(dbUrl);
+
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to the database");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.error("Database connection error:", err);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Disconnected from the database");
+  });
+};
+
+module.exports = { connectToMongo };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-const teste = true;
-console.log("222");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { connectToMongo } = require("./config/connect");
+const { User } = require("./users/schema");
+
+connectToMongo();
+app.use(cors({}));
+
+app.get("/", async (req, res) => {
+  const users = await User.find({});
+  return res.send({ users });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
